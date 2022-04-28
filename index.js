bdfe63aa3f13770aa464796cdf93a362feaab2fd
nosferatu-bodya <kresanbohdan420@gmail.com>
2022-04-28 16:58:06 +0300

    fix layout so it can contain a 14 letter word

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ function getRandomWord () {
     return words[Math.floor(Math.random() * words.length)]
 }
 
+function getMaxLenght () {
+    let max = words[0].length
+    for(let w of words){
+        w.length > max ? max = w.length : max = max
+    }
+    return max
+}
+
+console.log(getMaxLenght())
+
 // console.log(getRandomWord())
 // console.log(getRandomWord())
 // console.log(getRandomWord())
