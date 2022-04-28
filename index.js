f93ee8afea4cfd1a17d612cf85c7a9c68ea671a1
nosferatu-bodya <kresanbohdan420@gmail.com>
2022-04-28 16:54:36 +0300

    now you can get a random noun out of 1500 nouns

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import words from './words.js'
+
+function getRandomWord () {
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+// console.log(getRandomWord())
+// console.log(getRandomWord())
+// console.log(getRandomWord())
